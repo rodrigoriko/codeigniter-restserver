8368784fd9864dfd3f8d2db03738e8df444605e0
rodrigoriko <rodrigoriko@users.noreply.github.com>
2015-04-20 12:43:10 -0500

    Update REST_Controller.php
    Correction for check if is cli request. Last is_cli() now $this->input->is_cli_request().

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -950,7 +950,7 @@ abstract class REST_Controller extends CI_Controller
     protected function _parse_get()
     {
         // Fix for Issue #247
-        if (is_cli()) {
+        if ($this->input->is_cli_request()) {
             $args = $_SERVER['argv'];
             unset($args[0]);
             $_SERVER['QUERY_STRING'] =  $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI'] = '/' . implode('/', $args) . '/';
